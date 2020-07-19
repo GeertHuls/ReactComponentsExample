eae526282e3a0736a4e09881e06499b9eb844760
Geert Huls <geert.huls82@gmail.com>
2020-07-19 23:21:00 +0200

    Converted static images into html

--- src/pages/speakers.js
+++ src/pages/speakers.js
@@ -1,26 +1,10 @@
-import Menu from '../components/Menu/Menu';
-import Header from '../components/Header/Header';
-import SpeakerSearchBar from '../components/SpeakerSearchBar/SpeakerSearchBar';
 import Speakers from '../components/Speakers/Speakers';
-import Footer from '../components/Footer/Footer';
-import SpeakerContext from '../components/Speakers/SpeakerContext';
+import Layout from '../components/Layout/Layout';
 
 export default function Page() {
-  const speakers = [
-    { imageSrc: 'speaker-component-1124', name: 'Douglas Crockford' },
-    { imageSrc: 'speaker-component-1530', name: 'Tamara Baker' },
-    { imageSrc: 'speaker-component-10803', name: 'Eugene Chuvyrov' },
-];
-
   return (
-    <div>
-      <Header />
-      <Menu />
-      <SpeakerContext.Provider value={speakers}>
-        <SpeakerSearchBar />
-        <Speakers />
-      </SpeakerContext.Provider>
-      <Footer />
-    </div>
+    <Layout>
+      <Speakers />
+    </Layout>
   );
 }
