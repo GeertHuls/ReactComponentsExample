2900186f774658129ff8f028673bc805aaa0c938
Geert Huls <geert.huls82@gmail.com>
2020-07-23 00:31:30 +0200

    Moved reducer logic to dedicated files

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -3,6 +3,17 @@ import axios from 'axios';
 import Speaker from '../Speaker/Speaker';
 import SpeakerSearchBar from '../SpeakerSearchBar/SpeakerSearchBar';
 
+import requestReducer from '../../reducers/request';
+
+import {
+  GET_ALL_FAILURE,
+  GET_ALL_SUCCESS,
+  PUT_FAILURE,
+  PUT_SUCCESS,
+} from '../../actions/request';
+
+import { REQUEST_STATUS } from '../../reducers/request';
+
 const Speakers = () => {
 
   function toggleSpeakerFavorite(speakerRec) {
@@ -19,17 +30,13 @@ const Speakers = () => {
     try {
       await axios.put(`http://localhost:4000/speakers/${speakerRec.id}`, toggledSpeakerRec);
       dispatch({
-        speakers: [
-          ...speakers.slice(0, speakerIndex),
-          toggledSpeakerRec,
-          ...speakers.slice(speakerIndex + 1)
-        ],
-        type: 'GET_ALL_SUCCESS',
+        type: PUT_SUCCESS,
+        record: toggledSpeakerRec,
       });
     } catch (e) {
       dispatch({
-        status: REQUEST_STATUS.ERROR,
-        type: 'UPDATE_STATUS',
+        type: PUT_FAILURE,
+        error: e,
       });
       setError(e);
     }
@@ -37,34 +44,14 @@ const Speakers = () => {
 
   const [searchQuery, setSearchQuery] = useState('');
 
-  const REQUEST_STATUS = {
-    LOADING: 'loading',
-    SUCCESS: 'success',
-    ERROR: 'error'
-  };
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'GET_ALL_SUCCESS':
-        return {
-          ...state,
-          status: REQUEST_STATUS.SUCCESS,
-          speakers: action.speakers,
-        };
-      case 'UPDATE_STATUS':
-        return {
-          ...state,
-          status: action.status,
-        };
-    }
-  };
-
-  const [{ speakers, status }, dispatch] = useReducer(reducer, {
-    status: REQUEST_STATUS.LOADING,
-    speakers: [],
-  });
-
-  const [error, setError] = useState({});
+  const [{ records: speakers, status, error }, dispatch] = useReducer(
+    requestReducer,
+    {
+      records: [],
+      status: REQUEST_STATUS.LOADING,
+      error: null,
+    },
+  );
 
   // the useEffect callback function is called when the 
   // component is ready to be interacted with. This is similar
@@ -74,13 +61,13 @@ const Speakers = () => {
       try {
         const response = await axios.get('http://localhost:4000/speakers');
         dispatch({
-          speakers: response.data,
-          type: 'GET_ALL_SUCCESS',
+          type: GET_ALL_SUCCESS,
+          records: response.data,
         });
       } catch (e) {
         dispatch({
-          status: REQUEST_STATUS.ERROR,
-          type: 'UPDATE_STATUS',
+          type: GET_ALL_FAILURE,
+          error: e,
         });
         setError(e);
       }
