24c71a322e6a9648c8a48f5caaf5f6139969d567
Geert Huls <geert.huls82@gmail.com>
2020-07-23 23:40:09 +0200

    Use generic withRequest HOC to fetch data

--- src/components/Speakers/Speakers.js
+++ src/components/Speakers/Speakers.js
@@ -1,83 +1,21 @@
-import React, { useState, useEffect, useReducer } from 'react';
-import axios from 'axios';
+import React, { useState, } from 'react';
 import Speaker from '../Speaker/Speaker';
 import SpeakerSearchBar from '../SpeakerSearchBar/SpeakerSearchBar';
 
-import requestReducer from '../../reducers/request';
-import withRequest from '../HOCs/withRequest';
-
-import {
-  GET_ALL_FAILURE,
-  GET_ALL_SUCCESS,
-  PUT_FAILURE,
-  PUT_SUCCESS,
-} from '../../actions/request';
-
 import { REQUEST_STATUS } from '../../reducers/request';
 
-const Speakers = () => {
+import withRequest from '../HOCs/withRequest';
 
-  function toggleSpeakerFavorite(speakerRec) {
-    return {
+const Speakers = ({ records: speakers, status, error, put }) => {
+
+  const onFavoriteToggleHandler = async (speakerRec) => {
+    put({
       ...speakerRec,
       isFavorite: !speakerRec.isFavorite
-    };
-  }
-
-  async function onFavoriteToggleHandler(speakerRec) {
-    const toggledSpeakerRec = toggleSpeakerFavorite(speakerRec);
-    const speakerIndex = speakers.map(s => s.id).indexOf(speakerRec.id);
-
-    try {
-      await axios.put(`http://localhost:4000/speakers/${speakerRec.id}`, toggledSpeakerRec);
-      dispatch({
-        type: PUT_SUCCESS,
-        record: toggledSpeakerRec,
-      });
-    } catch (e) {
-      dispatch({
-        type: PUT_FAILURE,
-        error: e,
-      });
-      setError(e);
-    }
+    });
   }
 
   const [searchQuery, setSearchQuery] = useState('');
-
-  const [{ records: speakers, status, error }, dispatch] = useReducer(
-    requestReducer,
-    {
-      records: [],
-      status: REQUEST_STATUS.LOADING,
-      error: null,
-    },
-  );
-
-  // the useEffect callback function is called when the 
-  // component is ready to be interacted with. This is similar
-  // to componentDidMount if we were using React classes.
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('http://localhost:4000/speakers');
-        dispatch({
-          type: GET_ALL_SUCCESS,
-          records: response.data,
-        });
-      } catch (e) {
-        dispatch({
-          type: GET_ALL_FAILURE,
-          error: e,
-        });
-        setError(e);
-      }
-    }
-    fetchData();
-  }, [] /* This is the react hook dependeny array. This is a list of objects, state and
-    props, that when changed, cause a rerender of the page. For now this will be left empty,
-    which means that the function passed in as our first parameter will be executed once
-    when is done loading. */);
 
   const success = status === REQUEST_STATUS.SUCCESS;
   const isLoading = status === REQUEST_STATUS.LOADING;
@@ -113,4 +51,4 @@ const Speakers = () => {
     </div>
   );
 };
-export default withRequest()(Speakers);
+export default withRequest('http://localhost:4000', 'speakers')(Speakers);
